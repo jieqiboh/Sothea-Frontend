4fb6449ce29de9069172171d93b0d1cfe7254e05
Lim Pei En <limpeien123@gmail.com>
2024-05-18 23:36:01 +0800

    Add search bar and add new patient to all patients page

--- src/components/all patients page/AllPatientsTableModal.vue
+++ src/components/all patients page/AllPatientsTableModal.vue
@@ -3,8 +3,28 @@
         <div class="py-8">
             <div class="flex flex-row justify-between w-full mb-1 sm:mb-0">
                 <h2 class="text-4xl leading-tight" style="color:black">
-                    Patient Queue
+                    All Patients
                 </h2>
+            </div>
+
+            <div class="flex justify-between items-center" style="padding-top: 20px; padding-bottom: 20px">
+                <div class="relative flex-grow">
+                    <input 
+                        type="text" 
+                        id="&quot;form-subscribe-Filter" 
+                        class="rounded-lg border-transparent appearance-none bg-gray-300 border border-gray-300 py-3 px-4 text-gray-700 placeholder-gray-400 shadow-sm text-base focus:outline-none focus:ring-2 focus:ring-indigo-400 focus:border-transparent"
+                        placeholder="Search"
+                    />
+                </div>
+
+                <div class="flex items-center space-x-3">
+                    <Text style="font-size:medium">Add New Patient</Text>
+                    <span>
+                        <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-6 h-6">
+                            <path stroke-linecap="round" stroke-linejoin="round" d="M18 7.5v3m0 0v3m0-3h3m-3 0h-3m-2.25-4.125a3.375 3.375 0 1 1-6.75 0 3.375 3.375 0 0 1 6.75 0ZM3 19.235v-.11a6.375 6.375 0 0 1 12.75 0v.109A12.318 12.318 0 0 1 9.374 21c-2.331 0-4.512-.645-6.374-1.766Z" />
+                          </svg>                          
+                      </span>
+                </div>
             </div>
 
             <hr class="line"/> 
@@ -89,4 +109,5 @@ export default {
     height: 1px;
     background: rgba(0, 0, 0, 0.17);
 }
+
 </style>
