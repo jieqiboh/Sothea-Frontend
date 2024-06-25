11ab39276df2131c6d333a604c62bc76a608e1f1
jieqiboh <jieqiboh5836@gmail.com>
2024-06-26 03:49:42 +0800

    Temporary fix for slow page-loads due to attempts to load Inter font

--- tailwind.config.js
+++ tailwind.config.js
@@ -4,7 +4,8 @@ export default {
   theme: {
     extend: {
       fontFamily: {
-        inter: ['Inter', 'sans-serif']
+        // inter: ['Inter', 'sans-serif']
+        inter: ['sans-serif']
       },
       container: {
         padding: {
